0444c419f153c9c480eb6d3675ae7a055e9b6ca3
blue0777 <sohamsankpal9841@gmail.com>
2023-05-24 14:26:11 +0530

    train the model

---
+++ train.py
@@ -0,0 +1,76 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from PIL import Image
+
+dataset_dir = 'images'
+
+
+num_classes = 90  # Update the number of classes
+image_size = (150, 150)
+
+# Create an ImageDataGenerator for data augmentation and normalization expfor Soham to make #todo it
+datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2
+)
+
+train_generator = datagen.flow_from_directory(
+    dataset_dir,
+    target_size=image_size,
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator = datagen.flow_from_directory(
+    dataset_dir,
+    target_size=image_size,
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Create the CNN mode
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+try:
+    model.fit(
+        train_generator,
+        steps_per_epoch=train_generator.samples // train_generator.batch_size,
+        validation_data=validation_generator,
+        validation_steps=validation_generator.samples // validation_generator.batch_size,
+        epochs=10
+    )
+
+
+    model.save('model')
+
+    print("Model training and saving completed successfully!")
+
+except tf.errors.InvalidArgumentError as e:
+    print("InvalidArgumentError:", e)
+
+except Exception as e:
+    print("An error occurred during model training and saving:", e)
